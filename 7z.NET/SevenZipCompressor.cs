cc06098245d8a8c896b1df475b22d6b452fff164
Iulian <julian_marcu@yahoo.com>
2020-09-23 11:28:17 +0300

    Based on NuGet packageid="7z.NET" version="1.0.3", made 2 modifications:
    1. Extraction would not work when the DLL was used in a desktop application, that was ran from a shortcut, due to the fact of the way the path was read in
    SevenZipBase.cs, in the Path7za property. It was changed to get the location where the app was running.
    2. Removed the File.Exists check in SevenZipExtractor constructor, because it was failing on existing files.

---
+++ 7z.NET/SevenZipCompressor.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SevenZipNET.Arguments;
+
+namespace SevenZipNET
+{
+    /// <summary>
+    /// The compression level to use when compressing an archive.
+    /// </summary>
+    public enum CompressionLevel : int
+    {
+        /// <summary>
+        /// Stores files, performs no compression.
+        /// </summary>
+        Store = 0,
+        /// <summary>
+        /// The lowest and fastest amount of compression.
+        /// </summary>
+        Low = 1,
+        /// <summary>
+        /// The second lowest and fastest amount of compression.
+        /// </summary>
+        Fast = 3,
+        /// <summary>
+        /// The default amount of compression.
+        /// </summary>
+        Normal = 5,
+        /// <summary>
+        /// The second highest and slowest amount of compression.
+        /// </summary>
+        High = 7,
+        /// <summary>
+        /// The highest and slowest amount of compression.
+        /// </summary>
+        Ultra = 9
+    }
+
+    /// <summary>
+    /// A class to handle compressing files into an archive.
+    /// </summary>
+    public class SevenZipCompressor : SevenZipBase
+    {
+        private readonly string archive;
+        private readonly string password;
+        private readonly bool encryptHeaders;
+
+        /// <param name="filename">The path to the file to be created.</param>
+        public SevenZipCompressor(string filename)
+        {
+            archive = filename;
+        }
+
+        /// <param name="filename">The path to the file to be created.</param>
+        /// <param name="password">The password used in encryption.</param>
+        /// <param name="encryptHeaders">If set to true, encrypts file headers</param>
+        public SevenZipCompressor(string filename, string password, bool encryptHeaders = false)
+        {
+            archive = filename;
+            this.password = password;
+            this.encryptHeaders = encryptHeaders;
+        }
+
+
+        /// <summary>
+        /// Raised when progress of an operation has been updated.
+        /// </summary>
+        public event ProgressUpdatedEventArgs ProgressUpdated;
+
+        /// <summary>
+        /// Fires the ProgressUpdated event.
+        /// </summary>
+        /// <param name="progress">The current progress.</param>
+        protected void UpdateProgress(int progress)
+        {
+            ProgressUpdatedEventArgs invoke = ProgressUpdated;
+            if (invoke != null)
+            {
+                invoke(progress);
+            }
+        }
+
+        /// <summary>
+        /// Adds a directory to the archive, compressing it in the process.
+        /// </summary>
+        /// <param name="dir">The directory to compress.</param>
+        /// <param name="level">The amount of compression to use.</param>
+        public void CompressDirectory(string dir, CompressionLevel level = CompressionLevel.Normal)
+        {
+            CompressDirectory(dir, null, level);
+        }
+
+        /// <summary>
+        /// Adds a directory to the archive, compressing it in the process.
+        /// </summary>
+        /// <param name="dir">The directory to compress.</param>
+        /// <param name="workingdir">The working directory to set.</param>
+        /// <param name="level">The amount of compression to use.</param>
+
+        public void CompressDirectory(string dir, string workingdir, CompressionLevel level = CompressionLevel.Normal)
+        {
+            ArgumentBuilder argumentBuilder = GetCompressionArguments(level);
+
+            // Adding password switch only if password has been given in constructor
+            if (!string.IsNullOrEmpty(password))
+            {
+                argumentBuilder.AddSwitch(new SwitchPassword(password));
+
+                if (encryptHeaders)
+                {
+                    argumentBuilder.Add(new SwitchEncryptHeaders());
+                }
+            }
+            
+            argumentBuilder.AddTarget(dir);
+
+            WProcess p = new WProcess(argumentBuilder);
+
+            if (workingdir != null)
+            {
+                p.BaseProcess.StartInfo.WorkingDirectory = workingdir;
+            }
+
+            p.ProgressUpdated += ProgressUpdated;
+
+            p.Execute();
+        }
+
+        /// <summary>
+        /// Returns default arguments for compression.
+        /// </summary>
+        /// <param name="level">The amount of compression to use.</param>
+        /// <returns>ArgumentBuilder with default compression parameters.</returns>
+        private ArgumentBuilder GetCompressionArguments(CompressionLevel level)
+        {
+            return new ArgumentBuilder()
+                .AddCommand(SevenZipCommands.Add, archive)
+                .AddSwitch(new SwitchMultithread())
+                .AddSwitch(new SwitchOutputStream(OutputStream.Progress, OutputStreamState.Stdout))
+                .AddSwitch(new SwitchCompression(level));
+        }
+    }
+}
---
+++ 7z.NETTests/SevenZipExtractorTests.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SevenZipNET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenZipNET.Tests
+{
+    [DeploymentItem("test.7z")]
+    [DeploymentItem("test_encrypted.7z")]
+    [DeploymentItem("test_encrypted_encryptedheaders.7z")]
+    [DeploymentItem("testbroke.7z")]
+    [DeploymentItem("7za.exe")]
+    [DeploymentItem("7za64.exe")]
+    [TestClass()]
+    public class SevenZipExtractorTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            var ext = new SevenZipExtractor("test.7z");
+
+            Assert.AreEqual(6, ext.Files.Count);
+        }
+
+        [TestMethod()]
+        public void ExtractAllTest()
+        {
+            var ext = new SevenZipExtractor("C:\\Users\\Sergiu\\Desktop\\52f6adc0-e010-11ea-d38d-7522e9b4ebb0.zip");
+
+            string dir = Environment.CurrentDirectory + @"\ext\";
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            ext.ExtractAll(dir, true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 6);
+        }
+
+
+        [TestMethod()]
+        public void ExtractAllWithPasswordTest()
+        {
+            var ext = new SevenZipExtractor("test_encrypted.7z", "123");
+
+            string dir = Environment.CurrentDirectory + @"\ext\";
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            ext.ExtractAll(dir, true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 6);
+        }
+
+        [TestMethod()]
+        public void ExtractAllWithPasswordAndEncryptedHeadersTest()
+        {
+            var ext = new SevenZipExtractor("test_encrypted_encryptedheaders.7z", "123");
+
+            string dir = Environment.CurrentDirectory + @"\ext\";
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            ext.ExtractAll(dir, true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 6);
+        }
+
+        [TestMethod()]
+        public void ExtractWildcardTest()
+        {
+            var ext = new SevenZipExtractor("test.7z");
+
+            string dir = Environment.CurrentDirectory + @"\extw\";
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            ext.ExtractWildcard(dir, "*.cs", true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 4);
+
+            foreach (string s in Directory.EnumerateFiles(dir))
+                File.Delete(s);
+
+            ext.ExtractWildcard(dir, "7z*", true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 2);
+
+            foreach (string s in Directory.EnumerateFiles(dir))
+                File.Delete(s);
+
+            ext.ExtractWildcard(dir, "Core*.cs", true);
+
+            Assert.IsTrue(Directory.EnumerateFiles(dir).Count() == 1);
+
+            foreach (string s in Directory.EnumerateFiles(dir))
+                File.Delete(s);
+        }
+
+        [TestMethod()]
+        public void TestTest()
+        {
+            var ext = new SevenZipExtractor("ArchiveFileTests2.zip");
+
+            Assert.IsTrue(ext.TestArchive());
+        }
+
+        [TestMethod()]
+        public void TestTestBroken()
+        {
+            var ext = new SevenZipExtractor("test.7z");
+
+            Assert.IsTrue(ext.TestArchive());
+        }
+
+        [TestMethod()]
+        public void TestTestWithPassword()
+        {
+            var ext = new SevenZipExtractor("test_encrypted.7z", "123");
+
+            Assert.IsTrue(ext.TestArchive());
+        }
+
+        [TestMethod()]
+        public void TestTestWithPasswordAndEncryptedHeaders()
+        {
+            var ext = new SevenZipExtractor("test_encrypted_encryptedheaders.7z", "123");
+
+            Assert.IsTrue(ext.TestArchive());
+        }
+    }
+}
